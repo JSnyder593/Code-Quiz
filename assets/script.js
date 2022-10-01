7e97c64b23d7fdd44fb374764b9b78da64804bbe
JSnyder593 <jaeger_snyder@hotmail.com>
2022-10-01 15:33:43 -0700

    Basic HTML and CSS completed

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//Opening page
+    //Render page
+    //Start button
+        //event listener
+            //for loop to render first page
+
+//Question pages
+    //render page
+    //event listeners for taking the decision and storing boolean in local storage
+
+//Score is tallied
+    //take array/string from local storage and display score
+    //render text field to input initials/text and put in local storage for high score page
+
+//High score page
+    //display highscore & initials in an ordered list
