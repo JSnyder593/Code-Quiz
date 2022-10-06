b52509e675cccaa9ad389511e2189925308b7f4a
JSnyder593 <jaeger_snyder@hotmail.com>
2022-10-06 11:39:45 -0700

    functioning timer, functioning quiz card, functioning score tally, page moves properly until score and initials page

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,102 @@
-//section variables
+//Section Variables
 var quizIntro = document.getElementById('quiz-intro');
 var quizCard = document.getElementById('quiz-card');
 var scoreAndInitials = document.getElementById('score-and-initials');
 var highscorePage = document.getElementById('highscore-page');
 
-//timer
-var timerCount = document.getElementById('timer-count');
-var remainingTime = 45;//time in sec
-var intervalTimer = undefined;
+//Button Variables
+var startQuiz = document.getElementById('start-quiz');
 
-//initial state of sections when page loads
+var submitBtn = document.getElementById('submit-button');
+
+//Quiz Card
+var quizQuestion = document.getElementById('quiz-question');
+var answer1 = document.getElementById('answer1');
+var answer2 = document.getElementById('answer2');
+var answer3 = document.getElementById('answer3');
+var answer4 = document.getElementById('answer4');
+
+//Set initial state of sections
 quizIntro.style.display = 'initial';
 quizCard.style.display = 'none';
 scoreAndInitials.style.display = 'none';
 highscorePage.style.display = 'none';
 
-//Button Variables
-var startQuiz = document.getElementById('start-quiz');
+//Timer
+var timerCount = document.getElementById('timer-count');
+var remainingTime = 45;//time in sec
+var intervalTimer = undefined;
 
-//event listeners
+//Questions/Answers
+var question1 = 'Click Button 1';
+var question1Options = ['A', 'B', 'C', 'D'];
+var question1Correct = 0;
+var question2 = 'Click Button 2';
+var question2Options = ['E', 'F', 'G', 'H'];
+var question2Correct = 1;
+var question3 = 'Click Button 3';
+var question3Options = ['I', 'J', 'K', 'L'];
+var question3Correct = 2;
+var question4 = 'Click Button 4';
+var question4Options = ['M', 'N', 'O', 'P'];
+var question4Correct = 3;
+
+//Question State
+var currentQuestion = 0;
+var questions = [
+    [question1, question1Options, question1Correct],
+    [question2, question2Options, question2Correct],
+    [question3, question3Options, question3Correct],
+    [question4, question4Options, question4Correct],
+];
+
+//Score State
+var totalScore = 0;
+
+function updateQuizCard() {
+    quizQuestion.innerText = questions[currentQuestion][0];
+    answer1.innerText = questions[currentQuestion][1][0];
+    answer2.innerText = questions[currentQuestion][1][1];
+    answer3.innerText = questions[currentQuestion][1][2];
+    answer4.innerText = questions[currentQuestion][1][3];
+}
+
+function updateTimerDisplay() {
+    timerCount.innerText = remainingTime;
+    if (remainingTime <= 0) {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    quizCard.style.display = 'none';
+    scoreAndInitials.style.display = 'initial';
+    clearInterval(intervalTimer);
+    intervalTimer = undefined;
+    currentQuestion = 0;
+}
+
+function checkAnswer(currentAnswer) {
+    if (currentAnswer == questions[currentQuestion][2]) {
+        totalScore += 1;
+    } else {
+        remainingTime -= 10;
+        updateTimerDisplay();
+    }
+
+    currentQuestion += 1;
+    if(currentQuestion < questions.length) {
+        updateQuizCard();
+    } else {
+        endQuiz();
+    }
+}
+
+function timerEventHandler() {
+    remainingTime -= 1;
+    updateTimerDisplay();
+}
+
 startQuiz.addEventListener('click', (event) => {
     quizIntro.style.display = 'none';
     quizCard.style.display = 'initial';
@@ -28,3 +106,23 @@ startQuiz.addEventListener('click', (event) => {
     }
 });
 
+submitBtn.addEventListener('click', (event) =>{ 
+
+    
+});
+
+answer1.addEventListener('click', (event) => {
+    checkAnswer(0);
+});
+
+answer2.addEventListener('click', (event) => {
+    checkAnswer(1);
+});
+
+answer3.addEventListener('click', (event) => {
+    checkAnswer(2);
+});
+
+answer4.addEventListener('click', (event) => {
+    checkAnswer(3);
+});
